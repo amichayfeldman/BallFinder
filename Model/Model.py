d4202e8a83b391a4f8f7fdee9df6e5f175f8fbfe
amichayfeldman <amichayfeldman@gmail.com>
2020-08-12 06:38:51 +0300

    fix commas on Model.py

--- Model/Model.py
+++ Model/Model.py
@@ -5,29 +5,29 @@ class BallDetector(torch.nn.Module):
     def __init__(self):
         super(BallDetector, self).__init__()
         self.block1 = torch.nn.Sequential(torch.nn.Conv2d(in_channels=3, out_channels=8, kernel_size=5, stride=2),
-                                          torch.nn.BatchNorm2d(num_features=8)
+                                          torch.nn.BatchNorm2d(num_features=8),
                                           torch.nn.Conv2d(in_channels=8, out_channels=8, kernel_size=3),
-                                          torch.nn.BatchNorm2d(num_features=8)
+                                          torch.nn.BatchNorm2d(num_features=8),
                                           torch.nn.MaxPool2d(kernel_size=2))
 
         self.block2 = torch.nn.Sequential(torch.nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3),
-                                          torch.nn.BatchNorm2d(num_features=16)
+                                          torch.nn.BatchNorm2d(num_features=16),
                                           torch.nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3),
-                                          torch.nn.BatchNorm2d(num_features=16)
+                                          torch.nn.BatchNorm2d(num_features=16),
                                           torch.nn.MaxPool2d(kernel_size=2))
 
         self.upsample2 = torch.nn.ConvTranspose2d(in_channels=16, out_channels=16, kernel_size=2)
 
         self.block3 = torch.nn.Sequential(torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3),
-                                          torch.nn.BatchNorm2d(num_features=32)
+                                          torch.nn.BatchNorm2d(num_features=32),
                                           torch.nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3),
-                                          torch.nn.BatchNorm2d(num_features=32)
+                                          torch.nn.BatchNorm2d(num_features=32),
                                           torch.nn.MaxPool2d(kernel_size=2))
 
         self.upsample3 = torch.nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=4)
 
         self.block4 = torch.nn.Sequential(torch.nn.Conv2d(in_channels=56, out_channels=56, kernel_size=3),
-                                          torch.nn.BatchNorm2d(num_features=56)
+                                          torch.nn.BatchNorm2d(num_features=56),
                                           torch.nn.Conv2d(in_channels=56, out_channels=2, kernel_size=3))
 
     def forward(self, x):
